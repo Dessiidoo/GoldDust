fa75f77f5e246b21086699613482aab5c59a2d7a
Desidoo <93103573+Dessiidoo@users.noreply.github.com>
2026-08-13 21:20:55 -0400

    Fix Express SPA fallback for v5

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ app.get('/api/db/tables', async (_req, res) => {
 const distPath = path.resolve(__dirname, '../dist');
 app.use(express.static(distPath));
 
-app.get('*', (req, res, next) => {
+app.use((req, res, next) => {
   if (req.path.startsWith('/api/')) return next();
   res.sendFile(path.join(distPath, 'index.html'));
 });
